c219bf8d751d41c97c7fed5e2fc0cc02c875fddf
unknown <charlie890910@gmail.com>
2024-08-28 21:34:58 +0800

    第一次存檔

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+    public static void main(String[] args){
+       for(int a=1;a<=4;a++){
+            for(int b=1;b<=4;b++){
+                for(int c=1;c<=4;c++)
+                    System.out.print("@");
+                System.out.println();
+            }
+            System.out.println();
+       }
+    }
+
+}
