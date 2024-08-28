e9e3beb1d6534e61894ba06cff4e29ecc93c6291
unknown <charlie890910@gmail.com>
2024-08-28 21:39:49 +0800

    修改顯示內容

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test{
                     System.out.print("@");
                 System.out.println();
             }
-            System.out.println();
+            System.out.println(1);
        }
     }
 
